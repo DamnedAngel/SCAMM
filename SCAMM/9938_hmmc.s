66bea79f409bfc027db6be0416be4841b016ae1d
Damned Angel <danilo@angelo.eng.br>
2023-12-17 15:04:52 -0300

    HMMC working canonically
    HMMC working cannonically, with first byte being transmitted in command setup.

--- SCAMM/9938_hmmc.s
+++ SCAMM/9938_hmmc.s
@@ -6,105 +6,29 @@
 
 .include "vdpportmacros.s"
 
-.macro out_c_0
-.dw 0x71ed
-.endm
-
 .globl		_waitVdpReady
 
-
 ;----------------------------------------------------------
-;	Sets First HMMC (High speed move CPU to VRAM) of a HMMC 
-; 	chain up.
+;	Sets HMMC (High speed move CPU to VRAM) up.
 ;	Make sure to DI before calling
 ;	Input:
 ;		HL:		X pos (regs 36-37)
 ;		DE:		Y pos (regs 38-39)
+;		B: 		Direction (Reg 45)
 ;		HL':	Width (regs 40-41)
 ;		DE':	Height (regs 42-43)
-;		B:		Direction (reg 45)
+;		B':		First Color (reg 44)
 ;		Please check section 4.4.1 of 9938's manual
 ;	Output: C: 3rd VDP port
-;	Changes: AF, BC, HL
-; 	Note: possible to optimize unrolling _HMMCSetup.
-;----------------------------------------------------------
-_HMMCFirstSetup_::
-	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
-	exx
-	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
-	exx
-
-	call	_waitVdpReady
-	call	_HMMCSetup
-
-	exx
-
-
-;----------------------------------------------------------
-;	Sets First HMMC (High speed move CPU to VRAM) of a HMMC 
-; 	chain up.
-;	Make sure to DI before calling
-;	Input:
-;		HL:		X pos (regs 36-37)
-;		DE:		Y pos (regs 38-39)
-;		HL':	Width (regs 40-41)
-;		DE':	Height (regs 42-43)
-;		B:		Direction (reg 45)
-;		Please check section 4.4.1 of 9938's manual
-;	Output: C: 3rd VDP port
-;	Changes: AF, BC, HL
-; 	Note: possible to optimize unrolling _HMMCSetup.
-;----------------------------------------------------------
-_HMMCFirstSetup::
-	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
-	exx
-	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
-	exx
-
-	call	_waitVdpReady
-	ld		a, #36
-	vdp_OutA 1, _HMMCSetup			; 2nd port
-	ld		a, #17 + 0x80
-	vdp_OutA 1, _HMMCSetup			; R17 <- 36
-
-	out		(c), l					; Reg 36 (X lo)
-	out		(c), h					; Reg 37 (X hi)	
-	out		(c), e					; Reg 38 (Y lo)
-	out		(c), d					; Reg 39 (Y hi)
-	exx
-	
-	ld		a, #1
-	out		(c), a					; Reg 40 (Width lo) = 1
-	out_c_0							; Reg 41 (Width hi) = 0
-	out		(c), a					; Reg 42 (Height lo) = 1
-	out_c_0							; Reg 43 (Height hi) = 0
-	ld		a, #0xff
-	out		(c), a					; Reg 44 (Color, dummy) = 0xff
-	out_c_0							; Reg 45 (Direction) = 0
-
-	ld		a,	#0b11110000			; HMMC
-	out		(c), a					; Reg 46 (execute, dummy)
-
-	exx
-
-;----------------------------------------------------------
-;	Sets following HMMCs (High speed move CPU to VRAM) in a
-; 	HMMC chain up.
-;	Make sure to DI before calling, and to call _9938Stop 
-; 	after the last HMMC of the chain.
-;	Input:
-;		HL:		X pos (regs 36-37)
-;		DE:		Y pos (regs 38-39)
-;		HL':	Width (regs 40-41)
-;		DE':	Height (regs 42-43)
-;		B:		Direction (reg 45)
-; 		C: 		2nd port of the VDP.
-;		Please check section 4.4.1 of 9938's manual
-;	Output: C: 3rd VDP port
-;	Changes: AF, BC, HL
+;	Changes: AF, BC, HL, invert (EXX)
 ;----------------------------------------------------------
 _HMMCSetup::
+	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
+	exx
+	ld_c_vdpOutPort 3, _HMMCFirstSetup	; 4th port
+	exx
 
+	call	_waitVdpReady
 	ld		a, #36
 	vdp_OutA 1, _HMMCSetup			; 2nd port
 	ld		a, #17 + 0x80
@@ -112,16 +36,19 @@ _HMMCSetup::
 
 	out		(c), l					; Reg 36 (X lo)
 	out		(c), h					; Reg 37 (X hi)	
+
 	out		(c), e					; Reg 38 (Y lo)
 	out		(c), d					; Reg 39 (Y hi)
+	
 	exx
 	out		(c), l					; Reg 40 (Width lo)
 	out		(c), h					; Reg 41 (Width hi)
-	ld		a, e
-	inc		a							; avoid command to reach the end, allowing command chain
-	out		(c), a					; Reg 42 (Height lo (+1))
+
+	out		(c), e					; Reg 42 (Height lo (+1))
 	out		(c), d					; Reg 43 (Height hi)
-	out		(c), a					; Reg 44 (Color, dummy)
+	out		(c), b					; Reg 44 (Color, dummy)
+
+	exx
 	out		(c), b					; Reg 45 (Direction)
 
 	ld		a,	#0b11110000			; HMMC
@@ -132,6 +59,5 @@ _HMMCSetup::
 	vdp_OutA 1, _HMMCSetup			; 2nd port
 	ld		a, #17 + 0x80
 	vdp_OutA 1, _HMMCSetup			; R17 <- 44, no increment
-
+	
 	ret
-
--- TheTrial/hmmctest.s
+++ TheTrial/hmmctest.s
@@ -6,22 +6,21 @@
 
 .include "../SCAMM/vdpportmacros.s"
 
-.globl	_HMMCFirstSetup
 .globl	_HMMCSetup
-.globl  _9938Stop
 
 _HMMCTest::
 	di;
 	ld		hl, #16		; width
 	ld		de, #16		; height
-	ld		b, #0		; direction
+	ld		b, #0xfc	; first color
 	exx
 	ld		hl, #16		; x pos
 	ld		de, #16		; y pos
-	call	_HMMCFirstSetup
+	ld		b, #0		; direction
+	call	_HMMCSetup
 
-	ld		b, #0
-	ld		a, #255
+	ld		b, #0xff
+	ld		a, #3
 hhh:
 	out		(c), a
 	djnz	hhh
@@ -29,14 +28,14 @@ hhh:
 
 	ld		hl, #32		; width
 	ld		de, #8		; height
-	ld		b, #0		; direction
+	ld		b, #0x1c	; first color
 	exx
 	ld		hl, #58		; x pos
 	ld		de, #40		; y pos
-
+	ld		b, #0		; direction
 	call	_HMMCSetup
 
-	ld		b, #0
+	ld		b, #0xff
 	ld		a, #0xff
 ggg:
 	out		(c), a
@@ -45,21 +44,18 @@ ggg:
 next1:
 	ld		hl, #4		; width
 	ld		de, #64		; height
-	ld		b, #0		; direction
+	ld		b, #0xe0	; first color
 	exx
 	ld		hl, #200	; x pos
 	ld		de, #100	; y pos
-
+	ld		b, #0		; direction
 	call	_HMMCSetup
 
-	ld		b, #0
-	ld		a, #0xff
+	ld		b, #0xff
+	ld		a, #0x25
 ddd:
 	out		(c), a
 	djnz	ddd
-
-
-	call	_9938Stop
 
 	ei
 	ret
